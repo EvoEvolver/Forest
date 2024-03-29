33702101289cce768dba08a624b8773aca9e4d8e
Zijian Zhang <doomspec@outlook.com>
2024-03-29 19:10:14 -0400

    chore: update build checking for pip install

--- forest/__init__.py
+++ forest/__init__.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 current_file_directory = os.path.dirname(os.path.abspath(__file__))
 project_root = current_file_directory
@@ -9,28 +10,35 @@ asset_dir = os.path.join(build_dir, 'assets')  # Path to the assets directory
 def build():
     try:
         os.chdir(project_root)
-        os.system('npm install && npm run build')
+        subprocess.call(['npm', 'install'])
+        subprocess.call(['npm', 'run', 'build'])
         update_last_build_log()
     except Exception as e:
         print("Build Failed")
+        print(e)
 
-
-import subprocess
 
 def get_git_revision_hash() -> str:
-    try:
-        return subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=project_root).decode('ascii').strip()
-    except Exception:
+    # check if .git folder exists in the parent directory
+    parent_dir = os.path.dirname(project_root)
+    if not os.path.exists(f'{parent_dir}/.git'):
         return "non-git-repo-mode"
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=project_root).decode(
+        'ascii').strip()
 
 
 def lazy_build():
     # check if dist folder exists and the index.html file exists
     if check_if_need_rebuild():
         build()
+
+
 def check_if_need_rebuild() -> bool:
     # read last_build.txt if not return True
     try:
+        last_build_hash = get_git_revision_hash()
+        if last_build_hash == "non-git-repo-mode":
+            return not os.path.exists(f'{project_root}/dist')
         f = open(f'{project_root}/last_build.log', "r")
         last_build = f.read()
         if get_git_revision_hash() != last_build:
@@ -41,12 +49,12 @@ def check_if_need_rebuild() -> bool:
         print("file not found")
         return True
 
+
 def update_last_build_log():
     build_hash = get_git_revision_hash()
     f = open(f'{project_root}/last_build.log', "w")
     f.write(build_hash)
 
+
 if __name__ == '__main__':
-    # build()
-    # build_hash = get_git_revision_hash()
     lazy_build()
