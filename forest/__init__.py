d05ecaf73a483cc33b9b624817036d1a120405f5
Zijian Zhang <doomspec@outlook.com>
2024-06-17 11:32:04 -0400

    add download_new_dist

--- forest/__init__.py
+++ forest/__init__.py
@@ -8,29 +8,27 @@ build_dir = os.path.join(project_root, 'dist')
 asset_dir = os.path.join(build_dir, 'assets')  # Path to the assets directory
 dist_url = "https://github.com/EvoEvolver/Forest/raw/build/dist.tar.gz"
 
-def build():
-    os.chdir(project_root)
-    # check if dist folder exists
-    if os.path.exists(build_dir):
-        return
 
+def download_new_dist():
+    print("Forest: Downloading new dist...")
     wget.download(dist_url, 'dist.tar.gz')
-
     # Extract the tar file
     with tarfile.open('dist.tar.gz') as f:
         f.extractall()
-
     # Remove the tar file
     os.remove('dist.tar.gz')
+    print("Forest: New dist downloaded!")
+
 
 def lazy_build():
     # check if dist folder exists and the index.html file exists
     if check_if_need_rebuild():
-        build()
+        os.chdir(project_root)
+        download_new_dist()
 
 
 def check_if_need_rebuild() -> bool:
-    dist_exist = os.path.exists(f'{project_root}/dist')
+    dist_exist = os.path.exists(build_dir)
     if not dist_exist:
         return True
 
