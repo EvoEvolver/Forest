00b4b10288f4d687b57fd345e6f5edf505c30a03
Fangshi Du <125781993+dufangshi@users.noreply.github.com>
2024-10-27 17:15:23 -0400

    fix bugs, use card instead of directly merge contents

--- forest/src/FocusPage/SelectedNodeLayer.tsx
+++ forest/src/FocusPage/SelectedNodeLayer.tsx
@@ -14,7 +14,7 @@ import {Node} from "../entities";
 
 
 const NodeContentTabs = forwardRef(({
-                                        is_content_view,
+                                        leaves,
                                         tab_dict,
                                         env_funcs,
                                         env_vars,
@@ -31,6 +31,7 @@ const NodeContentTabs = forwardRef(({
     const handleChange = (event: React.SyntheticEvent, newValue: string) => {
         setValue(newValue);
     };
+
 
     useImperativeHandle(ref, () => ({
         setEmphasize: (em) => {
@@ -59,34 +60,78 @@ const NodeContentTabs = forwardRef(({
             backgroundColor: dark ? '#3b3d3e' : '#f4f4f4'
         }}>
             <CardContent>
-                {title && <Typography variant="h5" component="div" style={{color: dark ? 'white' : 'black'}}>
+                {!leaves && title && <Typography variant="h5" component="div" style={{color: dark ? 'white' : 'black'}}>
                     {title}
                 </Typography>}
-                {tab_keys.length > 0 &&
-                    <TabContext value={value} key={value}>
-                        <Box sx={{borderBottom: 0, borderColor: 'divider'}}>
-                            <TabList onChange={handleChange} aria-label="lab API tabs example">
-                                {tab_keys.map((tab, i) => {
-                                    return <Tab style={{color: dark ? 'white' : ''}} key={i} label={tab}
-                                                value={i.toString()}/>
-                                })}
-                            </TabList>
-                        </Box>
-                        {tab_keys.map((tab, i) => {
-                            return <TabPanel style={{paddingRight: "10px", paddingLeft: "10px", paddingTop: "5px"}}
-                                             value={i.toString()}>
-                                <Box sx={{overflowX: "auto", fontFamily: 'Verdana, sans-serif'}}>
-                                    {<JsxParser
-                                        bindings={{env_funcs, env_vars}}
-                                        components={env_components}
-                                        jsx={((tab !== 'content' && tab !== 'code') || !emphasize['enable']) ? (dark ? '<div style={{ color: \'white\' }}>' + tab_dict[tab] + '</div>' : tab_dict[tab]) : (dark ? '<div style={{ color: \'white\' }}>' + emphasizeText(tab_dict[tab], emphasize['keyword'], emphasize['wholeWord']) + '</div>' : emphasizeText(tab_dict[tab], emphasize['keyword'], emphasize['wholeWord']))}
-                                        renderError={error => <div
-                                            style={{color: "red"}}>{error.error.toString()}</div>}
-                                    />}</Box>
-                            </TabPanel>
-                        })}
-                    </TabContext>
+                {
+                    !leaves ? <>
+                        {tab_keys.length > 0 &&
+                            <>
+                                <TabContext value={value} key={value}>
+                                    <Box sx={{borderBottom: 0, borderColor: 'divider'}}>
+                                        <TabList onChange={handleChange} aria-label="lab API tabs example">
+                                            {tab_keys.map((tab, i) => {
+                                                return <Tab style={{color: dark ? 'white' : ''}} key={i} label={tab}
+                                                            value={i.toString()}/>
+                                            })}
+                                        </TabList>
+                                    </Box>
+                                    {tab_keys.map((tab, i) => {
+                                        return <TabPanel
+                                            style={{paddingRight: "10px", paddingLeft: "10px", paddingTop: "5px"}}
+                                            value={i.toString()}>
+                                            <Box sx={{overflowX: "auto", fontFamily: 'Verdana, sans-serif'}}>
+                                                {<JsxParser
+                                                    bindings={{env_funcs, env_vars}}
+                                                    components={env_components}
+                                                    jsx={((tab !== 'content' && tab !== 'code') || !emphasize['enable']) ? (dark ? '<div style={{ color: \'white\' }}>' + tab_dict[tab] + '</div>' : tab_dict[tab]) : (dark ? '<div style={{ color: \'white\' }}>' + emphasizeText(tab_dict[tab], emphasize['keyword'], emphasize['wholeWord']) + '</div>' : emphasizeText(tab_dict[tab], emphasize['keyword'], emphasize['wholeWord']))}
+                                                    renderError={error => <div
+                                                        style={{color: "red"}}>{error.error.toString()}</div>}
+                                                />}</Box>
+                                        </TabPanel>
+                                    })}
+                                </TabContext>
+                            </>
+                        }
+                    </> : <>
+                        {
+                            leaves.map((leaf, index) => (
+                                <>
+                                    <Typography variant="h6" component="div"
+                                                style={{color: dark ? 'white' : 'black'}}>
+                                        {leaf.title}
+                                    </Typography>
+                                    <TabContext value={value} key={value}>
+                                        <Box sx={{borderBottom: 0, borderColor: 'divider'}}>
+                                            <TabList onChange={handleChange} aria-label="lab API tabs example">
+                                                {Object.keys(leaf.tabs).map((tab, i) => {
+                                                    return <Tab style={{color: dark ? 'white' : ''}} key={i} label={tab}
+                                                                value={i.toString()}/>
+                                                })}
+                                            </TabList>
+                                        </Box>
+                                        {(Object.keys(leaf.tabs)).map((tab, i) => {
+                                            return <TabPanel
+                                                style={{paddingRight: "10px", paddingLeft: "10px", paddingTop: "5px"}}
+                                                value={i.toString()}>
+                                                <Box sx={{overflowX: "auto", fontFamily: 'Verdana, sans-serif'}}>
+                                                    {<JsxParser
+                                                        bindings={{env_funcs, env_vars}}
+                                                        components={env_components}
+                                                        jsx={((tab !== 'content' && tab !== 'code') || !emphasize['enable']) ? (dark ? '<div style={{ color: \'white\' }}>' + leaf.tabs[tab] + '</div>' : leaf.tabs[tab]) : (dark ? '<div style={{ color: \'white\' }}>' + emphasizeText(leaf.tabs[tab], emphasize['keyword'], emphasize['wholeWord']) + '</div>' : emphasizeText(leaf.tabs[tab], emphasize['keyword'], emphasize['wholeWord']))}
+                                                        renderError={error => <div
+                                                            style={{color: "red"}}>{error.error.toString()}</div>}
+                                                    />}</Box>
+                                            </TabPanel>
+                                        })}
+                                    </TabContext>
+                                </>
+
+                            ))
+                        }
+                    </>
                 }
+
             </CardContent>
         </Card>
     );
@@ -97,34 +142,11 @@ const SelectedNodeLayer = (props) => {
     let treeData = props.treeData;
     let layouter = props.layouter;
     const [animate, setAnimate] = useState(false);
-    const [currTabs, setCurrTabs] = useState({'content': 'hello'});
+    const [leaves, setLeaves] = useState(layouter.getAllLeaves(treeData));
+
     useEffect(() => {
 
-        console.log("selected node layer");
-        let leaves = layouter.getAllLeaves(treeData);
-        // for (const leaf of leaves) {
-        //     console.log(leaf);
-        // }
-        console.log("whole html:");
-        let i = 0;
-        let wholeHtmlCode = '';
-
-        for (const node of leaves) {
-            if (node.tabs && node.tabs['content']) {
-                wholeHtmlCode += `<div 
-                            data-ref="content-${i}"
-                            data-index="${node.id}"
-                            style="margin-bottom: 10px;"
-                        >`;
-                wholeHtmlCode += node.tabs['content'];
-                wholeHtmlCode += "</div>";
-                i++;
-            }
-        }
-
-        setCurrTabs({'content': wholeHtmlCode});
-
-        const intervalId = setInterval(() => {
+        const handleWheel = () => {
             const targets = document.querySelectorAll('[data-ref]');
             let left = 0;
             let right = targets.length - 1;
@@ -134,7 +156,7 @@ const SelectedNodeLayer = (props) => {
             // Define a helper function to get the distance of an element's rect.y from 0
             const getDistance = (target) => {
                 const rect = target.getBoundingClientRect();
-                return Math.abs(rect.y-300);
+                return Math.abs(rect.y - window.innerHeight * 0.2);
             };
 
             // Binary search for the minimum absolute rect.y
@@ -157,23 +179,51 @@ const SelectedNodeLayer = (props) => {
             }
             if (closestIndex !== -1) {
                 const nodeId = targets[closestIndex].getAttribute('data-index');
-                if(nodeId!==node.id){
+                if (nodeId !== node.id) {
                     props.modifyTree({
-                    type: 'setSelectedNode',
-                    id: targets[closestIndex].getAttribute('data-index')
-                });
+                        type: 'setSelectedNode',
+                        id: targets[closestIndex].getAttribute('data-index')
+                    });
                 }
-
             }
-        }, 100);
+
+        };
+        window.addEventListener('wheel', handleWheel);
+
 
         return () => {
-            clearInterval(intervalId);
+            window.removeEventListener('wheel', handleWheel);
         };
+    }, []);
+
+    const scrollToTarget = (id) => {
+        const targetElement = document.querySelector(`[data-index="${id}"]`);
+        if (targetElement) {
+            const rect = targetElement.getBoundingClientRect();
+            if(((rect.y+rect.height/2) > window.innerHeight*0.7) || ((rect.y+rect.height/2)<0) ) {
+                console.log(rect.y+rect.height/2, window.innerHeight*0.7)
+                targetElement.scrollIntoView({behavior: 'auto', block: 'center'});
+            }
+        }
+    };
+    useEffect(() => {
+        let i = 0;
+        for (const node of leaves) {
+            if (node.tabs && node.tabs['content']) {
+                node.tabs['content'] = `<div 
+                            data-ref="content-${i}"
+                            data-index="${node.id}"
+                            style="margin-bottom: 10px;"
+                        >` + node.tabs['content'] + "</div>";
+                i++;
+            }
+        }
+
     }, []);
 
     useEffect(() => {
         setAnimate(true);
+        scrollToTarget(node.id);
         const timeoutId = setTimeout(() => {
             setAnimate(false);
         }, 0);
@@ -197,40 +247,36 @@ const SelectedNodeLayer = (props) => {
 
     return (
         <Grid style={{height: "100%"}} container spacing={1}>
-            <Grid key={0} item xs={4} style={{
-                width: '30%',
+            <Grid key={0} item xs={3.5} style={{
                 height: "100%",
                 transition: animate ? 'opacity 0.5s ease-in' : 'none',
                 opacity: animate ? 0 : 1,
             }}>
-                <NodeContentTabs is_content_view={false} tab_dict={node.tools[0]} env_funcs={env_funcs}
+                <NodeContentTabs leaves={null} tab_dict={node.tools[0]} env_funcs={env_funcs}
                                  env_vars={env_vars}
                                  env_components={env_components} title="" dark={props.dark}/>
             </Grid>
 
-            <Grid key={1} item xs={4} style={{
-                width: '40%',
+            <Grid key={1} item xs={5} style={{
                 height: "100%",
                 transition: animate ? 'opacity 0.5s ease-in' : 'none',
                 opacity: animate ? 0 : 1,
             }}>
-                <NodeContentTabs is_content_view={true} tab_dict={currTabs} env_funcs={env_funcs} env_vars={env_vars}
+                <NodeContentTabs leaves={leaves} tab_dict={node.tools[0]} env_funcs={env_funcs} env_vars={env_vars}
                                  env_components={env_components} title={node.title} ref={props.contentRef}
                                  dark={props.dark}/>
             </Grid>
 
-            <Grid key={2} item xs={4} style={{
-                width: '30%',
-                height: '100%',
+            <Grid key={2} item xs={3.5} style={{
+                height: "100%",
                 transition: animate ? 'opacity 0.5s ease-in' : 'none',
                 opacity: animate ? 0 : 1,
             }}>
-                <NodeContentTabs is_content_view={false} tab_dict={node.tools[1]} env_funcs={env_funcs}
+                <NodeContentTabs leaves={null} tab_dict={node.tools[1]} env_funcs={env_funcs}
                                  env_vars={env_vars}
                                  env_components={env_components} title="" dark={props.dark}/>
             </Grid>
         </Grid>
-
     );
 };
 
