1a7cd06229a3ac73c0035c0d9e4afb6f111d988b
Zijian Zhang <doomspec@outlook.com>
2024-11-28 00:25:22 -0500

    chore: fix build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 npm run --prefix forest_server build
-rm -r forest/dist || true
+rm -r forest/dist 2> /dev/null
 cp -r forest_server/dist forest
 npm run --prefix forest_client build
 cp -r forest_client/dist forest/dist
