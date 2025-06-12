1b5d6342a68bd7eb7c2e9b2f4d175c54115098cf
Zijian Zhang <doomspec@outlook.com>
2025-06-12 18:40:25 -0400

    refactor: move Yjs setup logic to atoms and simplify App initialization

--- forest_client/src/App.tsx
+++ forest_client/src/App.tsx
@@ -2,70 +2,30 @@ import React, {useEffect, useState} from 'react';
 import {Box} from "@mui/material";
 import {ThemeProvider} from '@mui/material/styles';
 import {useAtom, useAtomValue, useSetAtom} from "jotai";
-import {
-    addNodeToNodeDictAtom,
-    deleteNodeFromNodeDictAtom,
-    selectedNodeIdAtom,
-    setTreeMetadataAtom
-} from "./TreeState/TreeState";
+import {setTreeMetadataAtom} from "./TreeState/TreeState";
 import TreeView from "./TreeView/TreeView";
-import {Map as YMap} from "yjs";
-import {setupYDoc, YDocAtom, YjsProviderAtom} from "./TreeState/YjsConnection";
+import {initSelectedNodeAtom, setupYDocAtom} from "./TreeState/YjsConnection";
 import LinearView from "./LinearView";
 import AuthModal from './UserSystem/AuthModal';
-import {updateChildrenCountAtom} from "./TreeState/childrenCount";
 import {themeOptions} from "./theme";
 import {subscriptionAtom, supabaseClientAtom} from "./UserSystem/authStates";
 import {getAppBar} from "./AppBar";
 import {treeId} from "./appState";
 
 
-const initSelectedNode = (ydoc, setSelectedNodeId) => {
-    let nodeId = new URLSearchParams(window.location.search).get("n");
-    const nodeDict = ydoc.getMap("nodeDict") as YMap<any>;
-    if (!nodeId) nodeId = ydoc.getMap("metadata").get("rootId") || null;
-    if (!nodeId) return;
-    // observe the nodeDict for changes
-    const observer = (ymapEvent) => {
-        if (ymapEvent.keys.has(nodeId)) {
-            setSelectedNodeId(nodeId);
-            // unobserve the nodeDict after setting the selected node
-            nodeDict.unobserve(observer);
-        }
-    }
-    nodeDict.observe(observer)
-    // unobserve the nodeDict after 10 seconds
-    setTimeout(() => {
-        nodeDict.unobserve(observer);
-    }, 10000);
-}
-
-
 export default function App() {
-    const setSelectedNodeId = useSetAtom(selectedNodeIdAtom)
-    const [, setYjsProvider] = useAtom(YjsProviderAtom)
-    const ydoc = useAtomValue(YDocAtom)
-    const addNodeToTree = useSetAtom(addNodeToNodeDictAtom)
-    const deleteNodeFromTree = useSetAtom(deleteNodeFromNodeDictAtom)
+    const initSelectedNode = useSetAtom(initSelectedNodeAtom)
     const setTreeMetadata = useSetAtom(setTreeMetadataAtom);
-    const updateChildrenCount = useSetAtom(updateChildrenCountAtom);
     const [subscription, setSubscription] = useAtom(subscriptionAtom);
     const [currentPage, setCurrentPage] = useState('tree');
     const supabaseClient = useAtomValue(supabaseClientAtom)
+    const setupYDoc = useSetAtom(setupYDocAtom);
 
     useEffect(() => {
         document.body.style.overflow = 'hidden';
-        //setDark(window.matchMedia('(prefers-color-scheme: dark)').matches);
-        // disable dark mode until we finish it
         if (treeId) {
-            setupYDoc(setYjsProvider, addNodeToTree, ydoc, deleteNodeFromTree, () => {
-                const treeMetadata = ydoc.getMap("metadata").toJSON()
-                console.log('Yjs sync completed', treeMetadata)
-                // Set up the metadata map
-                setTreeMetadata(treeMetadata)
-                updateChildrenCount({});
-            })
-            initSelectedNode(ydoc, setSelectedNodeId);
+            setupYDoc()
+            initSelectedNode();
             setTreeMetadata({
                 treeId: treeId
             })
--- forest_client/src/TreeState/YjsConnection.tsx
+++ forest_client/src/TreeState/YjsConnection.tsx
@@ -3,19 +3,36 @@ import {Map as YMap} from "yjs";
 import {atom, PrimitiveAtom} from "jotai";
 import {WebsocketProvider} from "y-websocket";
 import {treeId, wsUrl} from "../appState";
+import {addNodeToNodeDictAtom, deleteNodeFromNodeDictAtom, selectedNodeIdAtom, setTreeMetadataAtom} from "./TreeState";
+import {updateChildrenCountAtom} from "./childrenCount";
 
 export const YjsProviderAtom: PrimitiveAtom<WebsocketProvider> = atom();
 export const YDocAtom = atom(new Y.Doc());
+export const YjsConnectionStatusAtom = atom("connecting");
 
-export const setupYDoc = (setYjsProvider, addNodeToTree, ydoc, deleteNodeFromTree, observeSync) => {
+export const setupYDocAtom = atom(null, (get, set) => {
+    const ydoc = get(YDocAtom);
     let wsProvider = new WebsocketProvider(wsUrl, treeId, ydoc)
-    setYjsProvider(wsProvider)
+    set(YjsProviderAtom, wsProvider)
     wsProvider.on('status', event => {
-        console.log("Server connected!", event)
+        if (event.status === 'connected') {
+            set(YjsConnectionStatusAtom, "connected");
+        } else if (event.status === 'disconnected') {
+            set(YjsConnectionStatusAtom, "disconnected");
+        } else if (event.status === 'connecting') {
+            set(YjsConnectionStatusAtom, "connecting");
+        }
+        console.log('Yjs connection status:', event.status);
     })
     wsProvider.on('sync', isSynced => {
         if (isSynced) {
-            observeSync()
+            const treeMetadata = ydoc.getMap("metadata").toJSON()
+            console.log('Yjs sync completed', treeMetadata)
+            // Set up the metadata map
+            //setTreeMetadata(treeMetadata)
+            set(setTreeMetadataAtom, treeMetadata);
+            set(YjsConnectionStatusAtom, "connected");
+            set(updateChildrenCountAtom, {});
         }
     })
     let nodeDictyMap: YMap<YMap<any>> = ydoc.getMap("nodeDict")
@@ -23,14 +40,35 @@ export const setupYDoc = (setYjsProvider, addNodeToTree, ydoc, deleteNodeFromTre
         ymapEvent.changes.keys.forEach((change, key) => {
             if (change.action === 'add') {
                 let newNode = nodeDictyMap.get(key)
-                addNodeToTree(newNode)
+                set(addNodeToNodeDictAtom, newNode)
             } else if (change.action === 'update') {
                 let newNode = nodeDictyMap.get(key)
-                deleteNodeFromTree(key)
-                addNodeToTree(newNode)
+                set(deleteNodeFromNodeDictAtom, key)
+                set(addNodeToNodeDictAtom, newNode)
             } else if (change.action === 'delete') {
-                deleteNodeFromTree(key)
+                set(deleteNodeFromNodeDictAtom, key)
             }
         })
     })
-}
+})
+
+export const initSelectedNodeAtom = atom(null, (get, set) => {
+    const ydoc = get(YDocAtom);
+    let nodeId = new URLSearchParams(window.location.search).get("n");
+    const nodeDict = ydoc.getMap("nodeDict") as YMap<any>;
+    if (!nodeId) nodeId = ydoc.getMap("metadata").get("rootId") || null;
+    if (!nodeId) return;
+    // observe the nodeDict for changes
+    const observer = (ymapEvent) => {
+        if (ymapEvent.keys.has(nodeId)) {
+            set(selectedNodeIdAtom, nodeId);
+            // unobserve the nodeDict after setting the selected node
+            nodeDict.unobserve(observer);
+        }
+    }
+    nodeDict.observe(observer)
+    // unobserve the nodeDict after 10 seconds
+    setTimeout(() => {
+        nodeDict.unobserve(observer);
+    }, 10000);
+})
