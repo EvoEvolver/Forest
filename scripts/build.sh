b7c4cdda1df26562c4ab37b417278730c78b31b3
Zijian Zhang <doomspec@outlook.com>
2025-07-26 22:22:14 -0400

    chore: delete python version of forest toolkit

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 pnpm -r run build
-rm -r forest/dist 2> /dev/null
-cp -r packages/server/dist forest
-cp -r packages/client/dist forest/dist
-mv forest/dist/dist forest/dist/public
+rm -r build/dist 2> /dev/null
+mkdir -p build/dist
+cp -r packages/server/dist build
+cp -r packages/client/dist build/dist
+mv build/dist/dist build/dist/public
